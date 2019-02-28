694f49b88ec19125fe98fbe17363e5e88787bbc1
QH-20160928EBVB\Administrator <379257993@qq.com>
2019-02-28 16:32:21 +0800

    0228-5

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="index">
-    <!-- <div class="dex_user">
-      <img class="user_icon" src="../assets/images/userIcon.jpg"/>
-    </div> -->
+    <div class="dex_user">
+      <img class="user_icon" src="../assets/images/userIcon.jpg" />
+    </div>
     <div class="list">
       <div 
         class="li"
